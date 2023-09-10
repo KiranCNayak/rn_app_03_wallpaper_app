b607a8baee9c3bb7f032bec3830a9ede94335b8a
Kiran C Nayak <33034799+KiranCNayak@users.noreply.github.com>
2023-09-10 19:49:22 +0530

    First commit :tada:

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  plugins: ['prettier/prettier'],
+  rules: {
+    'array-bracket-spacing': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always'],
+  },
+};
---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
